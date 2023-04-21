643d71feaa08ab7b05054289d1e57c7c3f568777
Victoriia00 <55548301+Victoriia00@users.noreply.github.com>
2023-04-21 22:36:24 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,5 +5,5 @@ from scipy.stats import ttest_ind
 chat_id = 1099505768 # Ваш chat ID, не меняйте название переменной
 ## Воспользуемся T-test (дисперсия и матожидания конечны)
 def solution(x, y):
-    res = ttest_ind(x, y, equal_var=False, alternative='two-sided')
+    res = ttest_ind(x, y, equal_var=False, alternative = 'less')
     return res.pvalue < 0.06
