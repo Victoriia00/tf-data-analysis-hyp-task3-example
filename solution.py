5908bac40b13bfda646b667493cfc6242d35f8a1
Victoriia00 <55548301+Victoriia00@users.noreply.github.com>
2023-04-21 22:27:18 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,9 @@
 import pandas as pd
 import numpy as np
+from scipy.stats import ttest_ind
 
-
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
-
-def solution(...) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+chat_id = 1099505768 # Ваш chat ID, не меняйте название переменной
+## Воспользуемся T-test (дисперсия и матожидания конечны)
+def solution(x, y):
+    res = ttest_ind(x, y, equal_var=False, alternative='two-sided')
+    return res.pvalue < 0.06
